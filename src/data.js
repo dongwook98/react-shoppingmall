9705498b96975f1b1f1c613c59ec570903ba52b5
dongwook98 <ehddnr8563@gmail.com>
2023-03-20 01:39:57 +0900

    제품 상세설명 데이터 변경

--- src/data.js
+++ src/data.js
@@ -2,21 +2,21 @@ let data = [
   {
     id: 0,
     title: "White and Black",
-    content: "Born in France",
+    content: "이 신발은 화이트색과 검은색으로 이뤄어져 있습니다. ~~~~~~~~~~~~~~~~~~~~~~~~~~~",
     price: 120000
   },
 
   {
     id: 1,
     title: "Red Knit",
-    content: "Born in Seoul",
+    content: "이 신발은 주로 핑크색으로 이루어져 있습니다. ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
     price: 110000
   },
 
   {
     id: 2,
     title: "Grey Yordan",
-    content: "Born in the States",
+    content: "이 신발은 주로 회색으로 이루어져 있습니다. ~~~~~~~~~~~~~~~~~~~~~~~~~~~",
     price: 130000
   }
 ];

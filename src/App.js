7d706db3215540db6c2953f86aa65cff89c46a20
dongwook98 <ehddnr8563@gmail.com>
2023-03-19 18:21:54 +0900

    react-router-dom 라이브러리 학습

--- src/App.js
+++ src/App.js
@@ -8,60 +8,57 @@ import Col from 'react-bootstrap/Col';
 import data from './data.js' //데이터 불러오기
 import ProductList from './components/ProductList.jsx'; // 외부 파일에 있는 컴포넌트 파일 불러오기
 import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
 
   let [shoes] = useState(data);
 
   return (
-
     <div className="App">
-
-      <Navbar bg="primary" variant="dark" id='gnb-root'>
+      <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="#home" id='logo'>당근 마켓</Navbar.Brand>
-          <Nav className="me-auto menus">
-            <Nav.Link href="#home" className='menu'>중고거래</Nav.Link>
-            <Nav.Link href="#features" className='menu'>동네가게</Nav.Link>
-            <Nav.Link href="#pricing" className='menu'>알바</Nav.Link>
-            <Nav.Link href="#pricing" className='menu'>부동산 직거래</Nav.Link>
-            <Nav.Link href="#pricing" className='menu'>중고차 직거래</Nav.Link>
+          <Navbar.Brand href="#home">당근 마켓</Navbar.Brand>
+          <Nav className="me-auto">
+            <Link to="/" className='link'>홈</Link>
+            <Link to="/detail" className='link'>상세 페이지</Link>
+            <Link to="/cart" className='link'>장바구니</Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className='main-bg' style={{ backgroundImage: 'url(' + bg + ')' }}></div>
+      <Routes>
+        <Route path='/' element={
+          <Container>
+            <Row>
+              {/* <Col>
+              <ProductList shoes={shoes} index={0} src='https://codingapple1.github.io/shop/shoes1.jpg'></ProductList>
+            </Col>
+            <Col>
+              <ProductList shoes={shoes} index={1} src='https://codingapple1.github.io/shop/shoes2.jpg'></ProductList>
+            </Col>
+            <Col>
+              <ProductList shoes={shoes} index={2} src='https://codingapple1.github.io/shop/shoes3.jpg'></ProductList>
+            </Col> */}
+              <div className='main-bg' style={{ backgroundImage: 'url(' + bg + ')' }}></div>
 
-      <Container>
-        <Row>
-          {/* <Col>
-            <ProductList shoes={shoes} index={0} src='https://codingapple1.github.io/shop/shoes1.jpg'></ProductList>
-          </Col>
-          <Col>
-            <ProductList shoes={shoes} index={1} src='https://codingapple1.github.io/shop/shoes2.jpg'></ProductList>
-          </Col>
-          <Col>
-            <ProductList shoes={shoes} index={2} src='https://codingapple1.github.io/shop/shoes3.jpg'></ProductList>
-          </Col> */}
-
-          {
-            shoes.map((item, i) => {
-              return (
-                <Col>
-                  <ProductList shoes={shoes[i]} num={i}></ProductList>
-                  {/* shoes[i] : shoes 데이터를 다 전달하는게 아니라 한개씩 전달 */}
-                </Col>
-              )
-            })
-          }
-          {/* map함수로 컴포넌트 중복 제거 */}
-
-
-        </Row>
-      </Container>
-
-
-
+              {
+                shoes.map((item, i) => {
+                  return (
+                    <Col>
+                      <ProductList shoes={shoes[i]} num={i}></ProductList>
+                      {/* shoes[i] : shoes 데이터를 다 전달하는게 아니라 한개씩 전달 */}
+                    </Col>
+                  )
+                })
+              }
+              {/* map함수로 컴포넌트 중복 제거 */}
+            </Row>
+          </Container>
+        }></Route>
+        <Route path='/detail' element={<div>상세페이지임</div>} />
+        <Route path='/about' element={<div>어바웃페이지임</div>} />
+      </Routes>
     </div>
   );
 }

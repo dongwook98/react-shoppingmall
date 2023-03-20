d5c180645b5be8aac399ee1a61ae2e1966e2104b
dongwook98 <ehddnr8563@gmail.com>
2023-03-21 02:37:21 +0900

    useEffect 학습

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import styled from 'styled-components';
 import ProductList from '../components/ProductList';
@@ -6,7 +6,8 @@ import Col from 'react-bootstrap/Col';
 
 let Btn = styled.button`
   background: ${(props) => props.bg};
-  color: ${(props) => (props.bg === 'blue' ? 'white' : 'black')};
+  color: ${(props) => (props.bg === 'rgb(37,91,247)' ? 'white' : 'black')};
+  text-decoration: none;
   padding: 10px;
 `;
 
@@ -15,7 +16,21 @@ let Box = styled.div`
   padding: 20px;
 `;
 
+// 컴포넌트의 Lifecycle
+// 1. 페이지에 장착되기도 하고(mount)
+// 2. 가끔 업데이트도 되고 (update)
+// 3. 필요없으면 제거되고 (unmount)
+
 export default function Detail({ shoes }) {
+  useEffect(() => {
+    // mount, update시 코드 실행해주는 useEffect
+    console.log('안녕');
+    for (let i = 0; i < 10000; i++) {
+      console.log(1);
+    } // useEffect 사용하면 HTML렌더링 후에 실행됨!
+  });
+
+  let [count, setCount] = useState(0);
   let { userid } = useParams();
   let findProduct = shoes.find((product) => {
     return product.id === Number(userid);
@@ -24,6 +39,15 @@ export default function Detail({ shoes }) {
   if (userid) {
     return findProduct ? (
       <div className='container'>
+        {count}
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          버튼
+        </button>
+        <div className='alert alert-warning'>2초이내 구매시 할인</div>
         <Box>
           <Btn bg='blue'>버튼</Btn>
           <Btn bg='orange'>버튼</Btn>
@@ -63,7 +87,14 @@ export default function Detail({ shoes }) {
           <Col key={i}>
             <ProductList shoes={shoes[i]} num={i}></ProductList>
             {/* shoes[i] : shoes 데이터를 다 전달하는게 아니라 한개씩 전달 */}
-            <Link to={'/detail/' + i}>자세히 보기</Link>
+            <Btn bg='rgb(37,91,247)'>
+              <Link
+                style={{ textDecorationLine: 'none', color: 'white' }}
+                to={'/detail/' + i}
+              >
+                <span>자세히 보기</span>
+              </Link>
+            </Btn>
           </Col>
         );
       })}

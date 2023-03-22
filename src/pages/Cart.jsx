8893647e7902cc2db41b9a00f6674488cf177915
dongwook98 <ehddnr8563@gmail.com>
2023-03-22 23:10:18 +0900

    redux 셋팅

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
 
 export default function Cart() {
   return (
     <div>
-      <div>장바구니페이지</div>
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>상품명</th>
+            <th>수량</th>
+            <th>변경하기</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>안녕</td>
+            <td>안녕</td>
+            <td>안녕</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
